361e242ace1ffd881d51feaed927801a90ed65d4
B00164511 Cianan Carey <B00164511@mytudublin.ie>
2025-11-03 16:44:29 +0000

    step 7

--- firestoreConfig.js
+++ firestoreConfig.js
@@ -2,7 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // Your Firebase configuration object goes here
+  "projectId": "mawk8cianan",
+  "appId": "1:105859300929:web:9b8743734c123553abd0c0",
+  "storageBucket": "mawk8cianan.firebasestorage.app",
+  "apiKey": "AIzaSyD3gYvP0wVZJJtWpUhU14gO-tVAAsifGBw",
+  "authDomain": "mawk8cianan.firebaseapp.com",
+  "messagingSenderId": "105859300929",
+  "projectNumber": "105859300929",
+  "version": "2"
 }
 
 const app = initializeApp(firebaseConfig);
